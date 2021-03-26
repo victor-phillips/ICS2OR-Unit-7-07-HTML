2b6c0315f6cbacacbbaab0899e5c0feec0e1a7c4
victorphillips1 <victor.phillips@ocsbstudent.ca>
2021-03-26 13:33:21 +0000

    hopefully fixed some stuff

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ let loopnum = 0
 // event listener
 document.getElementById('button').addEventListener('click', calculate)
 // main function
-function calculate() {
+function calculate () {
   in1 = document.getElementById('in1').value
   in1 = parseInt(in1)
   in2 = document.getElementById('in2').value
   in2 = parseInt(in2)
   out = 0
   loopnum = 0
-while (loopnum !== in2) {
+  while (loopnum !== in2) {
     out = (in1 + out)
     loopnum = (loopnum + 1)
   }
